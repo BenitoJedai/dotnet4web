37de4b802324a8cb175bf6863c4b397a99b3b1b6
juan.manuel.lallana@gmail.com <juan.manuel.lallana@gmail.com@883b39f0-93ec-e68b-03ce-2a08be51d0a8>
2012-11-27 18:18:00 +0000

    Creando el trunk

---
+++ dotnet/n4w.ExampleProject/Main.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Threading;
+using Org.W3C;
+
+namespace n4w.ExampleProject
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Thread.Sleep(1000);
+			Window.Alert("Hello world!!!");
+		}
+	}
+}
