d8b0724307e1931fbe27b387a0c3b7c3ce2a123a
juan.manuel.lallana@gmail.com <juan.manuel.lallana@gmail.com@883b39f0-93ec-e68b-03ce-2a08be51d0a8>
2012-11-28 23:48:14 +0000

    Ejemplo mas complejo de clases

--- dotnet/n4w.ExampleProject/Main.cs
+++ dotnet/n4w.ExampleProject/Main.cs
@@ -12,4 +12,43 @@ namespace n4w.ExampleProject
 			Window.Alert("Hello world!!!");
 		}
 	}
+
+
+	public static class T1
+	{
+
+	}
+
+	public static class T2
+	{
+		public static void MA(){}
+
+	}
+	public static class T3
+	{
+		public static void MB(){}
+		public static void MC(){}
+	}
+	public static class T4
+	{
+
+	}
+	public static class T5
+	{
+		public static void MD(){}
+		public static void ME(){}
+		public static void MF(){}
+	}
+	public static class T6
+	{
+
+	}
+	public static class T7
+	{
+
+	}
+	public static class T8
+	{
+
+	}
 }
