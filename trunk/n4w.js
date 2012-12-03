1bd88c4286128392b03265891d0defc8c5199759
juan.manuel.lallana@gmail.com <juan.manuel.lallana@gmail.com@883b39f0-93ec-e68b-03ce-2a08be51d0a8>
2012-12-03 19:18:45 +0000

    Ahora los proyectos no a puntan a la mscorlib personalizada. Esto esta hecho para que compile en vs. La mscorlib igual hay que compilarla cuando se actualiza por que es la que se usa en serio.
    Para los usuario kde, la ejecucion del proyecto exe lanza un kdewebapp

--- trunk/n4w.js
+++ trunk/n4w.js
@@ -3,6 +3,42 @@
 
     "use strict";
 
+    if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+ 
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                                 ? this
+                                 : oThis,
+                               aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+ 
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+ 
+    return fBound;
+  };
+}
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     function toJsName(str)
     {
     	return str[0].toLowerCase() + str.substring(1);
@@ -529,8 +565,7 @@
             var isFat = byte() == 2;
         }
 
-        var blob = buffer.slice(metadataoffset + root.Streams.Blob.Offset,
-            metadataoffset + root.Streams.Blob.Offset + root.Streams.Blob.Size);
+        var blob =  root.Streams.Blob;
 
 
         function noparam(opcode)
