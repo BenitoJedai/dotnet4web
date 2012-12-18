a4e27fb1ca311ab926e6a5ce04125684edf2cb5a
juan.manuel.lallana@gmail.com <juan.manuel.lallana@gmail.com@883b39f0-93ec-e68b-03ce-2a08be51d0a8>
2012-12-18 16:44:47 +0000

    Implementada la parte basica de los metodos gordos

--- dotnet/n4w.ExampleProject/Main.cs
+++ dotnet/n4w.ExampleProject/Main.cs
@@ -7,19 +7,9 @@ namespace n4w.ExampleProject
 {
 	public class MainClass
 	{
-		public static void pruebaGenerico<T> ()
-		{
-
-		}
-
-		private static Node Create ()
-		{
-			return Window.Document.CreateTextNode("Hello world!!!");
-		}
-
 		public static void Main ()
 		{
-			Window.Document.Body.AppendChild(Create());
+			Window.Document.Body.AppendChild(Window.Document.CreateTextNode("Hello world!!!"));
 		}
 	}
 }
